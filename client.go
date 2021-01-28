898387f945cd4575f4a6f0ebca6bfc275a813e81
张俊超 <zhangjunchao@xiaohulu.com>
2021-01-28 14:33:56 +0800

    1.Add 新增etcd客户端支持TLS

--- client.go
+++ client.go
@@ -1,15 +1,17 @@
 package etcd
 
 import (
+	"crypto/tls"
 	"go.etcd.io/etcd/clientv3"
 	"time"
 )
 
 //NewEtcdClient etcd客户端创建
-func NewEtcdClient(endpoints []string) (*clientv3.Client, error) {
+func NewEtcdClient(endpoints []string, config *tls.Config) (*clientv3.Client, error) {
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   endpoints,
 		DialTimeout: 5 * time.Second,
+		TLS:         config,
 	})
 	if err != nil {
 		return nil, err

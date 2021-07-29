c078599429bb179dc0a5e547772743fbd5d07631
张俊超 <zhangjunchao@xiaohulu.com>
2021-07-29 18:49:30 +0800

    1.Update 更新注册服务的逻辑

---
+++ until.go
@@ -0,0 +1,17 @@
+package etcd
+
+func removeDuplicationMap(arr []string) []string {
+	set := make(map[string]struct{}, len(arr))
+	j := 0
+	for _, v := range arr {
+		_, ok := set[v]
+		if ok {
+			continue
+		}
+		set[v] = struct{}{}
+		arr[j] = v
+		j++
+	}
+
+	return arr[:j]
+}
